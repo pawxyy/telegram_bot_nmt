a86710ed1534170589bbd8620ffecf3e1cd06d8e
pawxyy <240109nb@gmail.com>
2026-02-11 16:58:26 +0200

    V2.3 add load_users and send_daily_message

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
-from zoneinfo import ZoneInfo
-from datetime import date
+from datetime import date, time
 from telegram import Update
 from telegram.ext import ApplicationBuilder, CommandHandler, ContextTypes
 import asyncio
 import random
+import pytz
 from bot_token import BOT_TOKEN
-from apscheduler.schedulers.asyncio import AsyncIOScheduler
 
 motivation_uk = ["Починати завжди варто з того, що сіє сумніви.",
                 "Найскладніше - почати.",
@@ -19,36 +18,64 @@ motivation_en = ["You should always start with what makes you doubt",
                 "Mistakes are part of the journey ",
                 "You can do it! I believe in you. Love u❤️️ "]
 
+
+def day_word_uk(n):
+    if 11 <= n % 100 <= 14:
+        return "днів"
+    elif n % 10 == 1:
+        return "день"
+    elif 2 <= n % 10 <= 4:
+        return "дні"
+    else:
+        return "днів"
+
+
+def day_word_en(n):
+    if n > 1:
+        return "days"
+    else:
+        return "day"
+
+nmt = date(2026, 6, 1)
+
+def get_days_left():
+    today = date.today()
+    return (nmt - today).days
+
+def load_users():
+    with open("user_id.txt", "r") as f:
+        return [int(line.strip()) for line in f]
+
+async def send_daily_message(context: ContextTypes.DEFAULT_TYPE):
+    users = load_users()
+    day_left = get_days_left()
+    for user_id in users:
+        try:
+            if day_left > 0:
+                await context.bot.send_message(chat_id=user_id,
+                                               text=f"До нмт лишилось {day_left} {day_word_uk(day_left)}")
+                await asyncio.sleep(1)
+
+                await context.bot.send_message(chat_id=user_id,
+                                               text=f"{random.choice(motivation_uk)}")
+            elif day_left == 0:
+                await context.bot.send_message(chat_id=user_id,
+                                               text="Сьогодні НМТ, бажаю успіху! Ти складеш всі 4 предмети на 200!!")
+            else:
+                await context.bot.send_message(chat_id=user_id,
+                                               text="Це моє останнє повідомлення, бажаю успіхів зі вступом :D")
+        except Exception as e:
+            print(f"Ошибка с {user_id}: {e}")
+
+kyiv_time = pytz.timezone("Europe/Kyiv")
+job_time = time(hour=16, minute=14, tzinfo=kyiv_time)
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
     chat_id = update.effective_user.id
-
-    with open ("user_id.txt", 'a+') as file:
-        file.seek(0)
-        users = file.read().splitlines()
-        if str(chat_id) not in users:
-            file.write(str(chat_id) + " \n ")
     user = update.effective_user
     lang = user.language_code
-    today = date.today()
-    nmt = date(2026, 6, 1)
-    day_left = (nmt - today).days
-
-    def day_word_uk(n):
-        if 11 <= n % 100 <= 14:
-            return "днів"
-        elif n % 10 == 1:
-            return "день"
-        elif  2<= n % 10 <= 4:
-            return "дні"
-        else:
-            return "днів"
-
-    def day_word_en(n):
-        if n > 1:
-            return "days"
-        else:
-            return "day"
-
+    day_left = get_days_left()
     if lang == "uk":
         if day_left > 0:
             await context.bot.send_message(chat_id= chat_id,
@@ -80,7 +107,5 @@ async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
 if __name__ == '__main__':
 
     app = ApplicationBuilder().token(BOT_TOKEN).build()
-    time_Kyiv = AsyncIOScheduler(timezone=ZoneInfo("Europe/Kyiv"))
-    time_Kyiv.start()
-    app.add_handler(CommandHandler("start", start))
+    app.job_queue.run_daily(send_daily_message, time=job_time)
     app.run_polling()
